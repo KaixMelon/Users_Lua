0871f90804c7c0d8d4f2e0d4094141007887017f
KaixMelon <macarubbokhervin74@gmail.com>
2025-09-06 23:56:10 +0800

    Injector users.lua update (2025-09-06 23:56:09)

--- users.lua
+++ users.lua
@@ -6,5 +6,7 @@ return {
         { name = "TestCurl", key = "TestCurl", expiry_datetime = "2025-09-07T22:43", banned = false },
         { name = "81d37c30a2d08c32", key = "81d37c30a2d08c32", expiry_datetime = "2025-09-07T22:45", banned = false },
         { name = "65593d659e8f73f3", key = "65593d659e8f73f3", expiry_datetime = "2025-09-07T23:54", banned = false },
+        { name = "06102a66095da6a2", key = "06102a66095da6a2", expiry_datetime = "2025-09-07T23:56", banned = false },
+        { name = "testlua", key = "testlua", expiry_datetime = "2025-09-07T23:56", banned = false },
     }
 }
